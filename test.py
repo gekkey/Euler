334cc34c0f4092fa25f17bb928659da816b4e760
Matt Geskey <matt@celtra.com>
2019-10-23 19:20:31 +0000

    added timing for each test case

--- test.py
+++ test.py
@@ -1,16 +1,24 @@
-import unittest, subprocess, os
+#!/usr/bin/env python3
+import os
+import time
+import subprocess
+import unittest
+
 
 class TestSolutions(unittest.TestCase):
     pass
 
+
 def make_test_case(num, answer):
     def test_case(self):
+        start = time.time()
         result = subprocess.run(
             'bin/' + num,
             stdout=subprocess.PIPE,
             encoding='utf-8',
-            #timeout=10
+            timeout=10
         )
+        duration = time.time() - start
         self.assertEqual(
             result.returncode,
             0
@@ -19,12 +27,14 @@ def make_test_case(num, answer):
             result.stdout.strip(),
             answer
         )
+        print(f"{num}: {int(duration * 1000)} ms")
     return test_case
 
+
 binfiles = sorted(os.listdir('bin/'))
-answers = [0] + open('answers.txt', 'r').read().split('\n')
+answers = [""] + open('answers.txt', 'r').read().split('\n')
 for f in binfiles:
-    if f == '000':
+    if f == '000':  # This is the scratch file
         continue
     a = answers[int(f)]
     setattr(TestSolutions, "test_" + f, make_test_case(f, a))
